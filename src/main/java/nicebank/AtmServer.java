4180ae28822df19c4948a73a8a91c0a45a429327
a.warzecha <a.warzecha89@gmail.com>
2017-10-12 19:25:35 +0200

    Server classes added

---
+++ src/main/java/nicebank/AtmServer.java
@@ -0,0 +1,36 @@
+package nicebank;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * Created by Anna on 2017-10-12.
+ */
+public class AtmServer {
+
+    private final Server server;
+
+    public AtmServer(int port) {
+        server = new Server(port);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(new AtmServlet()), "/*");
+    }
+
+    public void start() throws Exception {
+        server.start();
+        System.out.println("Listening on " + server.getURI());
+    }
+
+    public void stop() throws Exception {
+        server.stop();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new AtmServer(9988).start();
+    }
+}
---
+++ src/main/java/nicebank/AtmServlet.java
@@ -0,0 +1,21 @@
+package nicebank;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Anna on 2017-10-12.
+ */
+public class AtmServlet extends HttpServlet{
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println(
+                "<html><head><title>Nice Bank</title></head>"
+                  + "<body><h1>Welcome to our nice bank!</h1></body></html>");
+    }
+}
